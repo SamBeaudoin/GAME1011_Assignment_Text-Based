b542634568e8115834ab4246baa35912bceb7ee0
Ethan Dolman <EthanAndrew.Dolman@georgebrown.ca>
2021-04-07 13:26:07 -0400

    tweaked room class switch statements

--- Text_Based_Assignment/Room.cpp
+++ Text_Based_Assignment/Room.cpp
@@ -7,18 +7,22 @@ void Room::CreateRoom(char Direction, string Description)
 	switch(Direction)
 	{
 	case 'N':
+	case 'n':
 		if(North == nullptr)
 		North = new Room(Description);
 		break;
 	case 'E':
+	case 'e':
 		if (East == nullptr)
 			East = new Room(Description);
 		break;
 	case 'S':
+	case 's':
 		if (South == nullptr)
 			South = new Room(Description);
 		break;
 	case 'W':
+	case 'w':
 		if (West == nullptr)
 			West = new Room(Description);
 		break;
@@ -31,15 +35,19 @@ Room* Room::GetRoom(char Direction)
 	switch (Direction)
 	{
 	case 'N':
+	case 'n':
 		return North;
 		break;
 	case 'E':
+	case 'e':
 		return East;
 		break;
 	case 'S':
+	case 's':
 		return South;
 		break;
 	case 'W':
+	case 'w':
 		return West;
 		break;
 	}
